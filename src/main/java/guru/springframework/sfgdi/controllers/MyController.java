99365b2ee4353eff2c5288e9d63a495ad159d3c5
demo <0n11n3acc0un1@gmail.com>
2020-10-19 23:42:48 -0400

    dfdfd

---
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgdi.controllers;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello()
+    {
+        return "hello world";
+    }
+
+}
